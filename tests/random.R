bcb7d5f1af3ddd80b8861e4be6e7da16694da5ca
jeremiedb <jeremie_db@hotmail.com>
2019-06-26 20:17:21 -0400

    fix random

--- tests/random.R
+++ tests/random.R
@@ -15,16 +15,16 @@ xgb_train <- xgb.DMatrix(data = data_train, label = target_train)
 system.time(model <- xgb.train(data = xgb_train, params = params, nrounds = 10, verbose = 1, print_every_n = 10L, early_stopping_rounds = NULL))
 pred_xgb <- predict(model, xgb_train)
 
-params <- list(loss = "linear", nrounds = 10, eta = 0.05, lambda = 0, gamma = 0.5, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1, nbins = 50)
-system.time(model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("mae")))
+params <- list(loss = "linear", nrounds = 10, eta = 0.05, lambda = 0, gamma = 0.5, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1, nbins = 50, metric = "mae")
+system.time(model <- evo_train(data_train = data_train, target_train = target_train, params = params))
 pred_linear <- predict(model = model, data = data_train)
 
 params <- list(loss = "logistic", nrounds = 200, eta = 0.05, lambda = 0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("none"))
+model <- evo_train(data_train = data_train, target_train = target_train, params = params)
 pred_logistic <- predict(model = model, data = data_train)
 
 params <- list(loss = "poisson", nrounds = 200, eta = 0.05, lambda = 0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("none"))
+model <- evo_train(data_train = data_train, target_train = target_train, params = params)
 pred_poisson <- predict(model = model, data = data_train)
 
 data <- data.frame(x = x, y = y, pred_linear = pred_linear, pred_logistic = pred_logistic)

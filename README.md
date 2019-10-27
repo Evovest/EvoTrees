c30f68d7ef3bffa4a0e289d2a4ad0520d2a73248
jeremiedb <jeremie_db@hotmail.com>
2019-10-27 18:37:43 -0400

    move to version 0.3.1

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 EvoTrees
 ================
 
-> R package wrapping EvoTrees.jl, a pure Julia tree boosting library.
+> R package wrapping
+> [EvoTrees.jl](https://github.com/Evovest/EvoTrees.jl), a pure Julia
+> tree boosting library.
 
 # Installation
 
 EvoTrees.jl needs first to be installed and available from main Julia
-environment. Then, install EvoTree R package with:
+environment. It’s available on the official Julia registry. Can be
+installed from REPL: `] add EvoTrees`. Then, install EvoTrees R package:
 `devtools::install_github("Evovest/EvoTrees")`.
 
 # Parameters
 
-  - loss: {“linear”, “logistic”, “poisson”, “quantile”}
+  - loss: {“linear”, “logistic”, “poisson”, “L1”, “quantile”, “softmax”,
+    “gaussian”}
   - alpha: float, optional, \[0, 1\], set the quantile and L1 level,
     default=0.5
   - nrounds: 10L
@@ -23,13 +27,15 @@ environment. Then, install EvoTree R package with:
   - rowsample: float \[0,1\] default=1.0
   - colsample: float \[0,1\] default=1.0
   - nbins: integer \[2,250\] default=250
-  - metric: {“mse”, “rmse”, “logloss”, “quantile”}, default=“none”
+  - metric: {“mse”, “rmse”, “mae”, “logloss”, “mlogloss”, “poisson”,
+    “quantile”, “gaussian”}, default=“none”
 
 # Getting started
 
+## Regression
+
 ``` r
-# set data
-set.seed(123)
+set.seed(1234)
 x <- runif(10000, -6, 6)
 y <- sin(x) * 0.5 + 0.5
 y <- log(y/(1-y)) + rnorm(length(y))
@@ -52,18 +58,6 @@ params <- list(loss = "poisson", nrounds = 200, eta = 0.05, lambda = 0.5, gamma 
 model <- evo_train(data_train = data_train, target_train = target_train, params = params)
 pred_poisson <- predict(model = model, data = data_train)
 
-# quantile regression - q80
-params <- list(loss = "quantile", alpha=0.8, nrounds = 200, eta = 0.05, lambda = 0.0, gamma = 0.0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1, nbins=100)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params)
-pred_q80 <- predict(model = model, data = data_train)
-table(pred_q80 < target_train) / length(target_train)
-```
-
-    ## 
-    ##  FALSE   TRUE 
-    ## 0.8035 0.1965
-
-``` r
 # xgboost reference
 params <- list(max_depth = 4, eta = 0.05, subsample = 0.5, colsample_bytree = 1.0, min_child_weight = 1, lambda = 1, alpha = 0, gamma = 1.0, tree_method = "exact", objective = "reg:linear", eval_metric = "rmse")
 xgb_train <- xgb.DMatrix(data = data_train, label = target_train)
@@ -73,9 +67,10 @@ pred_xgb <- predict(model, xgb_train)
 
 ![](README_files/figure-gfm/unnamed-chunk-2-1.png)<!-- -->
 
+## Quantile
+
 ``` r
-# set data
-set.seed(123)
+set.seed(1234)
 x <- runif(10000, 0, 6)
 y <- sin(x) * 0.5 + 0.5
 y <- log(y/(1-y)) + rnorm(length(y))
@@ -84,36 +79,30 @@ data_train <- matrix(x)
 target_train <- y
 
 # quantile regression - q50
-params <- list(loss = "quantile", alpha=0.5, nrounds = 200, eta = 0.05, nbins=100, lambda = 1.0, gamma = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
+params <- list(loss = "quantile", alpha=0.5, nrounds = 200, eta = 0.05, nbins=100, lambda = 0.2, gamma = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
 model <- evo_train(data_train = data_train, target_train = target_train, params = params)
 pred_q50 <- predict(model = model, data = data_train)
 
 # quantile regression - q20
-params <- list(loss = "quantile", alpha=0.2, nrounds = 200, eta = 0.05, nbins=100, lambda = 1.0, gamma = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
+params <- list(loss = "quantile", alpha=0.2, nrounds = 200, eta = 0.05, nbins=100, lambda = 0.2, gamma = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
 model <- evo_train(data_train = data_train, target_train = target_train, params = params)
 pred_q20 <- predict(model = model, data = data_train)
 
 # quantile regression - q80
-params <- list(loss = "quantile", alpha=0.8, nrounds = 200, eta = 0.05, nbins=100, lambda = 1.0, gamma = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
+params <- list(loss = "quantile", alpha=0.8, nrounds = 200, eta = 0.05, nbins=100, lambda = 0.2, gamma = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
 model <- evo_train(data_train = data_train, target_train = target_train, params = params)
 pred_q80 <- predict(model = model, data = data_train)
 
 # lightgbm
-params <- list(objective = "quantile", alpha=0.2, eta = 0.05, max_bin=100, l1=0, l2=0.5, min_split_gain = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
+params <- list(objective = "quantile", alpha=0.2, eta = 0.05, max_bin=100, l1=0, l2=0.1, min_split_gain = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
 dtrain <- lgb.Dataset(data = data_train, label = target_train, free_raw_data = FALSE)
 model <- lightgbm::lgb.train(params = params, data = dtrain, nrounds = 100)
 pred_q20_lgb <- predict(model, data_train)
 
-params <- list(objective = "quantile", alpha=0.8, eta = 0.05, max_bin=100, l1=0, l2=0.5, min_split_gain = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
+params <- list(objective = "quantile", alpha=0.8, eta = 0.05, max_bin=100, l1=0, l2=0.1, min_split_gain = 0.0, max_depth = 4, min_weight = 1, rowsample = 0.5, colsample = 1)
 dtrain <- lgb.Dataset(data = data_train, label = target_train, free_raw_data = FALSE)
 model <- lightgbm::lgb.train(params = params, data = dtrain, nrounds = 100)
 pred_q80_lgb <- predict(model, data_train)
-
-# table(target_train < pred_q50) / length(target_train)
-# table(target_train < pred_q20) / length(target_train)
-# table(target_train < pred_q80) / length(target_train)
-# table(target_train < pred_q20_lgb) / length(target_train)
-# table(target_train < pred_q80_lgb) / length(target_train)
 ```
 
-![](README_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+![](README_files/figure-gfm/unnamed-chunk-6-1.png)<!-- -->
--- tests/random.R
+++ tests/random.R
@@ -2,7 +2,7 @@ library(EvoTrees)
 library(ggplot2)
 library(xgboost)
 
-nrows <- 100000
+nrows <- 200000
 ncols = 100
 x <- matrix(runif(nrows * ncols), nrow = nrows)
 y <- runif(nrows)
@@ -10,70 +10,16 @@ target_train <- y
 data_train <- x
 
 # no regularisation
-params <- list(nthreads = 4, max_depth = 5, eta = 0.05,subsample = 0.5, colsample_bytree = 1.0, min_child_weight = 1, lambda = 0, alpha = 0, gamma = 0, tree_method = "hist", objective = "reg:linear", max_bin=16)
+params <- list(nthreads = 4, max_depth = 5, eta = 0.05, subsample = 0.5, colsample_bytree = 0.5, min_child_weight = 1, lambda = 0, alpha = 0, tree_method = "hist", objective = "reg:linear", eval_metric = "rmse", max_bin=32)
 xgb_train <- xgb.DMatrix(data = data_train, label = target_train)
-system.time(model <- xgb.train(data = xgb_train, params = params, nrounds = 10, verbose = 1, print_every_n = 10L, early_stopping_rounds = NULL))
+system.time(model <- xgb.train(data = xgb_train, params = params, nrounds = 100, verbose = 1, print_every_n = 2L, early_stopping_rounds = NULL))
 pred_xgb <- predict(model, xgb_train)
 
-params <- list(loss = "linear", nrounds = 10, eta = 0.05, lambda = 0, gamma = 0.01, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1, nbins = 16)
+params <- list(loss = "linear", nrounds = 100, eta = 0.05, lambda = 0, gamma = 0.01, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 0.5, nbins = 32)
 system.time(model <- evo_train(data_train = data_train, target_train = target_train, params = params, print_every_n = 10))
 pred_linear <- predict(model = model, data = data_train)
 
-params <- list(loss = "logistic", nrounds = 200, eta = 0.05, lambda = 0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params)
+params <- list(loss = "logistic", nrounds = 100, eta = 0.05, lambda = 0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 0.5)
+system.time(model <- evo_train(data_train = data_train, target_train = target_train, params = params))
 pred_logistic <- predict(model = model, data = data_train)
 
-params <- list(loss = "poisson", nrounds = 200, eta = 0.05, lambda = 0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params)
-pred_poisson <- predict(model = model, data = data_train)
-
-data <- data.frame(x = x, y = y, pred_linear = pred_linear, pred_logistic = pred_logistic)
-ggplot() + geom_point(aes(x=x, y=y), col = "gray", size = 0.5) +
-  geom_line(aes(x=x, y=pred_linear, col = "linear"), size = 1) +
-  geom_line(aes(x=x, y=pred_logistic, col = "logistic"), size = 1) +
-  geom_line(aes(x=x, y=pred_poisson, col = "poisson"), size = 1) +
-  geom_line(aes(x=x, y=pred_xgb, col = "xgb"), size = 1) +
-  ggtitle("No penalty")
-
-
-# l2 regularization
-params <- list(max_depth = 5, eta = 0.05,subsample = 0.5, colsample_bytree = 1.0, min_child_weight = 1, lambda = 1.0, alpha = 0, gamma = 0,tree_method = "exact", objective = "reg:linear", eval_metric = "rmse")
-xgb_train <- xgb.DMatrix(data = data_train, label = target_train)
-system.time(model <- xgb.train(data = xgb_train, params = params, nrounds = 200, verbose = 1, print_every_n = 10L, early_stopping_rounds = NULL))
-pred_xgb <- predict(model, xgb_train)
-
-params <- list(loss = "linear", nrounds = 200, eta = 0.05, lambda = 1.0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-system.time(model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("mse")))
-pred_linear <- predict(model = model, data = data_train)
-
-params <- list(loss = "logistic", nrounds = 200, eta = 0.05, lambda = 1.0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("logloss"))
-pred_logistic <- predict(model = model, data = data_train)
-
-data <- data.frame(x = x, y = y, pred_linear = pred_linear, pred_logistic = pred_logistic)
-ggplot() + geom_point(aes(x=x, y=y), col = "gray", size = 0.5) +
-  geom_line(aes(x=x, y=pred_linear, col = "linear"), size = 1) +
-  geom_line(aes(x=x, y=pred_logistic, col = "logistic"), size = 1) +
-  geom_line(aes(x=x, y=pred_xgb, col = "xgb"), size = 1) +
-  ggtitle("L2 regularization")
-
-# gamma/pruning regularization
-params <- list(max_depth = 5, eta = 0.05,subsample = 0.5, colsample_bytree = 1.0, min_child_weight = 1, lambda = 0, alpha = 0, gamma = 1.0, tree_method = "exact", objective = "reg:linear", eval_metric = "rmse")
-xgb_train <- xgb.DMatrix(data = data_train, label = target_train)
-model <- xgb.train(data = xgb_train, params = params, nrounds = 200, verbose = 1, print_every_n = 10L, early_stopping_rounds = NULL)
-pred_xgb <- predict(model, xgb_train)
-
-params <- list(loss = "linear", nrounds = 200, eta = 0.05, lambda = 0, gamma = 1.0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("mse"))
-pred_linear <- predict(model = model, data = data_train)
-
-params <- list(loss = "logistic", nrounds = 200, eta = 0.05, lambda = 0, gamma = 1.0, max_depth = 6, min_weight = 1, rowsample = 0.5, colsample = 1)
-model <- evo_train(data_train = data_train, target_train = target_train, params = params, metric = as.symbol("logloss"))
-pred_logistic <- predict(model = model, data = data_train)
-
-data <- data.frame(x = x, y = y, pred_linear = pred_linear, pred_logistic = pred_logistic)
-ggplot() + geom_point(aes(x=x, y=y), col = "gray", size = 0.5) +
-  geom_line(aes(x=x, y=pred_linear, col = "linear"), size = 1) +
-  geom_line(aes(x=x, y=pred_logistic, col = "logistic"), size = 1) +
-  geom_line(aes(x=x, y=pred_xgb, col = "xgb"), size = 1) +
-  ggtitle("Gamma pruning")
